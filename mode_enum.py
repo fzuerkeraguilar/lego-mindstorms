46bd165931f98f29d7ca2cbd96a9addb96859c1d
Fabian Zürker Aguilar <fzuerker@gmail.com>
2022-11-08 14:20:12 +0100

    Add Enum for different modes

---
+++ mode_enum.py
@@ -0,0 +1,13 @@
+from linefollower import LineFollower
+from debug import Debug
+from boxpusher import BoxPusher
+from bridgecrosser import BridgeCrosser
+from pointfinder import PointFinder
+
+class MODES:
+    LINE_FOLLOWER = LineFollower
+    DEBUG = Debug
+    BOX_PUSHER = BoxPusher
+    BRIDGE_CROSSER = BridgeCrosser
+    POINT_FINDER = PointFinder
+    
