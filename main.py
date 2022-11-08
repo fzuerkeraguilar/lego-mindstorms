56886614b44eb0e5ba2b2ecd7e6a7bdf4202123a
Fabian Zürker Aguilar <fzuerker@gmail.com>
2022-11-08 14:23:05 +0100

    Put main in class

--- main.py
+++ main.py
@@ -11,42 +11,48 @@ from linefollower import LineFollower
 from debug import Debug
 from turning_distance_sensor import TurningDistanceSensor
 
+class Main:
 
-ev3 = EV3Brick()
-right_motor = Motor(Port.B)
-left_motor = Motor(Port.A)
-distance_drive = Motor(Port.C)
-drive_base = DriveBase(left_motor, right_motor, wheel_diameter=33, axle_track=185)
-color_sensor = ColorSensor(Port.S1)
-r_touch_sensor = TouchSensor(Port.S2)
-l_touch_sensor = TouchSensor(Port.S3)
-distance_sensor = TurningDistanceSensor(Port.C, Port.S4)
+    def __init__(self):
+        self.ev3 = EV3Brick()
+        self.right_motor = Motor(Port.B)
+        self.left_motor = Motor(Port.A)
+        self.distance_drive = Motor(Port.C)
+        self.drive_base = DriveBase(left_motor, right_motor, wheel_diameter=33, axle_track=185)
+        self.color_sensor = ColorSensor(Port.S1)
+        self.r_touch_sensor = TouchSensor(Port.S2)
+        self.l_touch_sensor = TouchSensor(Port.S3)
+        self.distance_sensor = TurningDistanceSensor(Port.C, Port.S4)
 
-# Menu to select the program to run
-ev3.screen.load_image(ImageFile.QUESTION_MARK)
-ev3.speaker.beep()
-# Wait for a button to be pressed
-while not any(ev3.buttons.pressed()):
-    wait(10)
-    if Button.CENTER in ev3.buttons.pressed():
+    def main():
+        # Menu to select the program to run
+        ev3.screen.load_image(ImageFile.QUESTION_MARK)
         ev3.speaker.beep()
-        Debug(drive_base, color_sensor, r_touch_sensor, distance_sensor, ev3, right_motor, left_motor, distance_drive, 100).run()
-        break
+        # Wait for a button to be pressed
+        while not any(ev3.buttons.pressed()):
+            wait(10)
+            if Button.CENTER in ev3.buttons.pressed():
+                ev3.speaker.beep()
+                Debug(self.drive_base, self.color_sensor, self.r_touch_sensor, self.distance_sensor, self.ev3,
+                    self.right_motor, self.left_motor, self.distance_drive).run()
+                break
 
-    elif Button.UP in ev3.buttons.pressed():
-        ev3.speaker.beep()
-        LineFollower(drive_base, right_motor, left_motor, color_sensor, r_touch_sensor, distance_sensor, ev3).run()
-        break
+            elif Button.UP in ev3.buttons.pressed():
+                ev3.speaker.beep()
+                LineFollower(self.drive_base, self.right_motor, self.left_motor, self.color_sensor, self.r_touch_sensor, self.distance_sensor, self.ev3).run()
+                break
 
-    elif Button.RIGHT in ev3.buttons.pressed():
-        ev3.speaker.beep()
-        break
+            elif Button.RIGHT in ev3.buttons.pressed():
+                ev3.speaker.beep()
+                break
 
-    elif Button.DOWN in ev3.buttons.pressed():
-        ev3.speaker.beep()
-        break
+            elif Button.DOWN in ev3.buttons.pressed():
+                ev3.speaker.beep()
+                break
 
-    elif Button.LEFT in ev3.buttons.pressed():
-        ev3.speaker.beep()
-        break
+            elif Button.LEFT in ev3.buttons.pressed():
+                ev3.speaker.beep()
+                break
 
+if __name__ == "__main__":
+    Main().main()
