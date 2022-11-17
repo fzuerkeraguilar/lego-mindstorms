6cef790a3c6a6fab8436842f81e0fdbfc8742872
Max Umbach <=>
2022-11-17 12:16:53 +0100

    added function to remove redundancy

--- modes/linefollower.py
+++ modes/linefollower.py
@@ -5,6 +5,7 @@ from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, UltrasonicSenso
 from pybricks.tools import wait, StopWatch
 from modes.mode import Mode
 
+
 class LineFollower(Mode):
     BLACK = 6
     WHITE = 42
@@ -18,16 +19,24 @@ class LineFollower(Mode):
     INITIAL_TURN = 50
     END_COLOR = Color.BLUE
 
-    def __init__(self, ev3_hub, drivebase, right_motor, left_motor,
-    color_sensor, distance_sensor, speed=INITIAL_SPEED):
+    def __init__(
+        self,
+        ev3_hub,
+        drivebase,
+        right_motor,
+        left_motor,
+        color_sensor,
+        distance_sensor,
+        speed=INITIAL_SPEED,
+    ):
         super().__init__(ev3_hub, drivebase, color_sensor, distance_sensor, speed)
         self.right_motor = right_motor
         self.left_motor = left_motor
 
-    #TODO: Switch from reflection to rgb to detect the blue marker
+    # TODO: Switch from reflection to rgb to detect the blue marker
     def follow_line(self):
         if self.distance_sensor.distance() < 100:
-                self.avoid_obstacle()
+            self.avoid_obstacle()
 
         # Calculate the deviation from the threshold.
         reflection = self.color_sensor.reflection()
@@ -37,7 +46,7 @@ class LineFollower(Mode):
             self.hub.speaker.beep()
             self.drivebase.stop()
             return True
-        
+
         if reflection <= self.BLACK + 3:
             self.speed = self.INITIAL_SPEED
             if not self.find_line_direct():
@@ -51,7 +60,7 @@ class LineFollower(Mode):
         turn_rate = self.GAIN * deviation
         self.drivebase.drive(self.speed, turn_rate)
         return False
-    
+
     def avoid_obstacle(self):
         self.drivebase.stop()
         self.drivebase.turn(90)
@@ -62,39 +71,63 @@ class LineFollower(Mode):
         self.drivebase.straight(200)
         self.drivebase.turn(90)
         self.find_line_direct()
-    
-    def find_line_direct(self):
 
+    def turn_and_find_line(self, speed, time, turn_right):
         watch = StopWatch()
         watch.pause()
         watch.reset()
+        if turn_right:
+            speed_right = -speed
+            speed_left = speed
+            self.hub.screen.print("Turn right")
+
+        else:
+            speed_right = speed
+            speed_left = -speed
+            self.hub.screen.print("Turn left")
 
         self.hub.speaker.beep()
         self.drivebase.stop()
-        self.hub.screen.print("Turn left")
-        self.right_motor.run_time(500, 1300, then=Stop.HOLD, wait=False)
-        self.left_motor.run_time(-500, 1300, then=Stop.HOLD, wait=False)
-        watch.resume()
-        while watch.time() < 1400:
+
+        self.right_motor.run_time(right_speed, time, then=Stop.HOLD, wait=False)
+        self.left_motor.run_time(left_speed, time, then=Stop.HOLD, wait=False)
+
+        while watch.time() < time + 100:
             if self.color_sensor.reflection() > self.THRESHOLD + 3:
                 self.hub.screen.print("Found line 1")
                 self.right_motor.stop()
                 self.left_motor.stop()
                 return True
-            pass 
-        
-        self.hub.screen.print("Turn right")
-        self.right_motor.run_time(-500, 2600, then=Stop.HOLD, wait=False)
-        self.left_motor.run_time(500, 2600, then=Stop.HOLD, wait=False)
-        watch.reset()
-        while watch.time() < 2700:
-            if self.color_sensor.reflection() > self.THRESHOLD + 3:
-                self.hub.screen.print("Found line 2")
-                self.right_motor.stop()
-                self.left_motor.stop()
-                return True
             pass
-        
+
+        return False
+
+    def find_line_direct(self):
+
+        # watch = StopWatch()
+        # watch.pause()
+        # watch.reset()
+
+        # self.hub.speaker.beep()
+        # self.drivebase.stop()
+        # self.hub.screen.print("Turn left")
+        # self.right_motor.run_time(500, 1300, then=Stop.HOLD, wait=False)
+        # self.left_motor.run_time(-500, 1300, then=Stop.HOLD, wait=False)
+        # watch.resume()
+        # while watch.time() < 1400:
+        #     if self.color_sensor.reflection() > self.THRESHOLD + 3:
+        #         self.hub.screen.print("Found line 1")
+        #         self.right_motor.stop()
+        #         self.left_motor.stop()
+        #         return True
+        #     pass
+
+        if self.turn_and_find_line(500, 1300, True):
+            return True
+
+        if self.turn_and_find_line(500, 2600, False):
+            return True
+
         self.right_motor.run_time(500, 1400, then=Stop.HOLD, wait=False)
         self.left_motor.run_time(-500, 1400, then=Stop.HOLD, wait=True)
         return False
@@ -132,5 +165,3 @@ class LineFollower(Mode):
         while Button.CENTER not in self.hub.buttons.pressed():
             if self.follow_line():
                 break
-            
-        
