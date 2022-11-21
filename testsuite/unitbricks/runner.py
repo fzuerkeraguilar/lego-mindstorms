ed784148f5b97033a29b53fec81ee5ab27b0b4d1
Daniel <dani.ritz@gmx.de>
2022-11-21 18:04:13 +0100

    parametrized tests

--- testsuite/unitbricks/runner.py
+++ testsuite/unitbricks/runner.py
@@ -36,8 +36,52 @@ def run_module(path):
 
 def run_test(modname, module, test):
     print(f'\n== Test {modname}/{test}"\n')
+    params_name = "params_" + test
+    if params_name in dir(module):
+        params = getattr(module, params_name)()
+        return run_test_with_params(module, test, params)
+    else:
+        return run_simple_test(module, test)
+
+def run_simple_test(module, test):
+    func = getattr(module, test)
+    try:
+        reset_environment()
+        result = func()
+        if result == None:
+            return True
+        else:
+            return result
+    except Exception as ex:
+        print('Error:', ex)
+        return False
+
+def run_test_with_params(module, test, params):
+    res = True
+    func = getattr(module, test)
+    for param in params:
+        try:
+            print(f"Params:", param)
+            reset_environment()
+            result = func(param)
+            if result == False:
+                res = False
+        except Exception as ex:
+            print(f'Error:', ex)
+            res = False
+    return res
+
+
+
+def run_test_values(modname, module, test, value):
+    print(f'\n== Test {modname}/{test}"\n')
 
     func = getattr(module, test)
+    values_name = "values_" + test
+    values = None
+    if values_name in dir(module):
+        values = getattr(module, values_name)
+    print(test, values)
     try:
         reset_environment()
         result = func()
