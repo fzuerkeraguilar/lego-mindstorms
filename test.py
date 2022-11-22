350485f845cd383b57237b82d01afba4839115d7
Daniel <dani.ritz@gmx.de>
2022-11-22 11:59:28 +0100

    moved unitbricks internal tests to unitbricks pkg

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 #!./test
-from unitbricks.runner import run_tests
+from unitbricks.runner import run_tests, run_module
+import sys
 
-run_tests("tests")
+TEST_PATH = "tests"
+args = sys.argv
+args.pop(0) # first element is command name
+
+if len(args) > 0:
+    for arg in args:
+        path = f"{arg.replace('.', '/')}.test.py"
+        run_module(path)
+
+else:
+    run_tests(TEST_PATH)
