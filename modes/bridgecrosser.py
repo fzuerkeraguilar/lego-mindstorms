a68b703218029693010b20e5d4ac69e1831a3c04
Fabian Zürker Aguilar <fzuerker@gmail.com>
2022-12-12 13:21:35 +0100

    Use distance_sensor

--- modes/bridgecrosser.py
+++ modes/bridgecrosser.py
@@ -12,37 +12,63 @@ class BridgeCrosser(Mode):
     INITIAL_SPEED = 50
     WALL_DISTANCE = 572
     RAMP_LENGTH = 1050
+    BRIDGE_LENGTH = 1050
 
     def __init__(self, color_sensor, distance_sensor, config, speed=INITIAL_SPEED):
         super().__init__(color_sensor, distance_sensor, config, speed)
         self.drivebase = config.get_drivebase(clean=True)
 
     def run(self):
-        self.drive_up()
+        self.drive_up_ramp()
         self.drivebase.turn(-90)
-        self.drive_straight()
+        self.drive_straight(1260)
         self.drivebase.turn(-90)
-        self.drive_down()
+        self.drive_down_ramp()
 
-    def drive_up(self):
-        self.drivebase.straight(1050)
-
-    def drive_straight(self):
-        target_distance = 1260
-        speed = 100
-
+    def drive_up_ramp(self):
         self.drivebase.reset()
-        self.drivebase.drive(speed, 0)
-
-        music = self.play_music()
-        
-        while self.drivebase.distance() < target_distance:
-            next(music)
-
+        self.drivebase.drive(self.INITIAL_SPEED, 0)
+        while self.drivebase.distance() < self.RAMP_LENGTH:
+            if self.color_sensor.reflection() == 0:
+                self.drivebase.stop()
+                self.drivebase.straight(-40)
+                return
+            distance = self.distance_sensor.distance()
+            self.hub.screen.print("Distance: ", distance)
+            if distance > 150:
+                self.drivebase.drive(self.INITIAL_SPEED, -15)    
+            else :
+                self.drivebase.drive(self.INITIAL_SPEED, 1)
         self.drivebase.stop()
 
-    def drive_down(self):
-        self.drivebase.straight(1000)
+    def drive_straight(self, drive_distance, speed=100):
+        self.distance_sensor.set_angle(0)
+        self.drivebase.reset()
+        self.drivebase.drive(speed, 0)
+        
+        while self.drivebase.distance() < drive_distance:
+            distance = self.distance_sensor.distance()
+            self.hub.screen.print("Distance: ", distance)
+            if distance > 150:
+                self.drivebase.drive(speed, -15)    
+            else :
+                self.drivebase.drive(speed, 1)
+        self.drivebase.stop()
+
+    def drive_down_ramp(self):
+        self.drivebase.reset()
+        self.drivebase.drive(self.INITIAL_SPEED, 0)
+        while self.drivebase.distance() < self.RAMP_LENGTH:
+            if self.color_sensor.color() == Color.BLUE:
+                self.drivebase.stop()
+                return
+            distance = self.distance_sensor.distance()
+            self.hub.screen.print("Distance: ", distance)
+            if distance > 1500:
+                self.drivebase.drive(self.INITIAL_SPEED, -10)    
+            else :
+                self.drivebase.drive(self.INITIAL_SPEED, 1)
+        self.drivebase.stop()
 
     def play_music(self):
         frequency = [
@@ -63,7 +89,7 @@ class BridgeCrosser(Mode):
 
         for i in range(0, len(frequency)):
             self.hub.screen.print("Beep", frequency[i])
-            # self.hub.speaker.beep(frequency[i], duration[i])
+            self.hub.speaker.beep(frequency[i], duration[i])
             yield
         
         while True:
